3eceae402996d6203227c1e96db8d94559ae8b2b
tlkhanh <longkhanhta@gmail.com>
2024-01-27 11:58:55 +0700

    init project

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+const lessons=[
+  {
+    path: '/',
+    text: 'Setup'
+  },
+  {
+    path: '/method',
+    text: 'Method'
+  },
+  {
+    path: '/computed',
+    text: 'Computed'
+  },
+  {
+    path: '/reactive-syntax',
+    text: 'Reactive Syntax'
+  },
+  {
+    path: '/modular',
+    text: 'Module'
+  },
+  {
+    path: '/lifecycle',
+    text: 'Lifecycle'
+  },
+  {
+    path: '/watch',
+    text: 'Watch'
+  },
+  {
+    path: '/sharing-state',
+    text: 'Sharing State'
+  },
+  {
+    path: '/susspense',
+    text: 'Susspense'
+  },
+  {
+    path: '/teleport',
+    text: 'Teleport'
+  },
+];
+
+</script>
+
+<template>
+  <div>
+    <header>
+      <nav>
+        <RouterLink v-for="lesson of lessons" :to="lesson.path">{{ lesson.text }}</RouterLink>
+      </nav>
+  </header>
+  <RouterView />
+  </div>
+</template>
+
+<style scoped>
+</style>
