9b0de42f0ac69fae0b6c82bae3200c7d3b331490
tlkhanh <longkhanhta@gmail.com>
2024-01-28 21:52:49 +0700

    susspense

---
+++ src/components/EmployeeList.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+async function fetchData() 
+{  
+    return new Promise(async (resolve) => {    
+        const res = await fetch(`https://hub.dummyapis.com/employee?noofRecords=10&idStarts=1001`);    
+        setTimeout(async () => resolve(await res.json()), 1000);  
+    });
+}
+
+const data: any = await fetchData()
+
+</script>
+
+<template>
+<table>
+    <tr>
+        <th>
+            <h2>First Name</h2>
+        </th>
+        <th>
+            <h2>Last Name</h2>
+        </th>
+    </tr>
+<tr v-for="e in data">
+ <td>{{ e.firstName }}</td>
+ <td>{{ e.lastName }}</td>
+</tr>
+</table>
+</template>
+
+<style lang="scss">
+
+</style>
--- src/views/SusspenseView.vue
+++ src/views/SusspenseView.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
-
+import EmployeeList from "@/components/EmployeeList.vue";
+import { onErrorCaptured, ref } from "vue";
+const error: any = ref(null);
+onErrorCaptured((e, instance, info) => {
+    error.value=e;
+    return true
+});
 </script>
 
 <template>
-Susspense views works!
+  <div v-if="error">Error: {{ error }}</div>
+  <Suspense v-else>
+    <EmployeeList />
+    <template #fallback>Loading employees...</template>
+  </Suspense>
 </template>
 
-<style scoped>
-
-</style>
+<style lang="scss"></style>
