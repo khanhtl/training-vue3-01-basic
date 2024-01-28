f178c84774769d148ed97938752b9257d45ffe86
tlkhanh <longkhanhta@gmail.com>
2024-01-28 22:51:32 +0700

    generic

---
+++ src/components/Generic.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts" generic="T extends string | number, U extends Todo">
+import type { Todo } from '@/composables/useTodoService';
+
+
+defineProps<{
+    id: T,
+    items: U[]
+}>();
+
+
+
+</script>
+
+<template>
+<div>
+    <h1>ID: {{ id }}</h1>
+    <ul>
+      <li v-for="item in items">
+        {{ item.text }}
+    </li>  
+    </ul>
+</div>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/views/GenericView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Generic from '@/components/Generic.vue';
+const todos=[
+    {
+        id: 1,
+        text: 'Working',
+        completed: false
+    }
+]
+</script>
+
+<template>
+<Generic :id="100" :items="todos"></Generic>
+</template>
+
+<style lang="scss">
+
+</style>
