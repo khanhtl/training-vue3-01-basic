1f8975d98c36a263cb7ee2b1980f78946bf17817
tlkhanh <longkhanhta@gmail.com>
2024-01-28 22:02:01 +0700

    teleport

---
+++ src/components/Popup.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const emits=defineEmits<{
+    close: []
+}>()
+</script>
+
+<template>
+    <div class="popup-container">
+        <div class="popup-content">
+            <button @click="emits('close')" class="popup-close-btn">Đóng</button>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+.popup-container {
+    position: fixed;
+    inset: 0;
+    background: rgba( #000000,  0.1);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .popup-content {
+        position: absolute;
+        width: 560px;
+        height: 400px;
+        background: #fff;
+        padding: 16px;
+        .popup-close-btn {
+            position: absolute;
+            right: 16px;
+            top: 16px;
+            cursor: pointer;
+        }
+    }
+}
+</style>
--- src/views/TeleportView.vue
+++ src/views/TeleportView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import Popup from '@/components/Popup.vue';
+import { ref } from 'vue';
+
+const isShowPopup=ref(false);
 
 </script>
 
 <template>
-Teleport views works!
+<button @click="isShowPopup = true">Show popup</button>
+<Teleport to="body">
+
+    <Popup @close="isShowPopup = false" v-if="isShowPopup" />
+</Teleport>
 </template>
 
 <style scoped>
